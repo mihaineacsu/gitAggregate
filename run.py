28df2e89b38911d2a5f3c7b6845e4402280b0d03
mihaineacsu <mihai.neacsu@ymail.com>
2013-06-06 18:23:43 +0300

    Fixes #25; adds command line args to run.py

--- run.py
+++ run.py
@@ -1,12 +1,40 @@
 #!/usr/bin/env python
 
-import tornado.ioloop
+import sys
 from gitAggregate import app, loop
 
-if __name__ == "__main__":
+usage = 'Usage: '
+
+def listen(port=8888):
     """
-        Listens on local port 8888.
+        Listens on local port.
         Runs indefinitely.
     """
     app.listen(8888)
     loop.start()
+
+def run_command(command):
+    """
+        Switch based on given command line argument.
+        Returns refernce to function that is to be run.
+    """
+    return {
+        'query' : query,
+        'listen': listen,
+    }.get(command, None)
+
+def query(p):
+    pass
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+
+    if not args:
+        print usage
+    else:
+        foo = run_command(args[0])
+
+        if foo is None:
+            print usage
+        else:
+            foo(sys.argv[1:])
